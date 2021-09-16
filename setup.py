c0ca6133226eb9c6c578bc36ae37fbd7c9d4b2f8
Oran Gilboa <oran.gilboa1@gmail.com>
2021-09-16 19:31:19 +0300

    inc version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name='marrs',
-	version='1.0.2',
+	version='1.0.3',
 	author='Oran Gilboa',
 	author_email='oran.gilboa1@gmail.com',
 	package_dir={'': 'src'},
